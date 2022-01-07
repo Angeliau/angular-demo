8869d0b48555079fd8c88c3fba6ef79cd5fbd2b4
Angeliau <42534525+Angeliau@users.noreply.github.com>
2022-01-07 21:37:26 +0800

    提交form模块源码

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,1 +1,44 @@
+import { Component, OnInit } from '@angular/core';
 
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+
+  public personInfo:any={
+    username:'',
+    sex:'1',
+    cityList:['北京','上海','广州','深圳','杭州'],
+    city:'北京',
+    hobby:[{
+      title:'旅游',
+      checked:true
+    },{
+      title:'唱歌',
+      checked:false
+    },{
+      title:'跑步',
+      checked:false
+    }],
+    mark:''
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  doSubmit(){
+    // dom操作
+    /*
+    let username:any = document.getElementById("username");
+    console.log(username.value);
+    */
+
+    //双向数据绑定
+    console.log(this.personInfo);
+  }
+
+}
