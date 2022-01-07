b0ee77c27a90ffe8d533fcba13c14bab8224b5c5
Angeliau <42534525+Angeliau@users.noreply.github.com>
2022-01-07 21:43:55 +0800

    提交search模块源码

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,1 +1,36 @@
+import { Component, OnInit } from '@angular/core';
 
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+  public keyword:string='';
+  public historyList:any[]=[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  doSearch(){
+    if (this.historyList.indexOf(this.keyword) == -1){
+      if (this.historyList.indexOf(" ") != -1){
+        this.keyword="";
+      }else{
+        this.historyList.push(this.keyword);
+      }
+    }
+    
+    this.keyword='';
+    //console.log(this.keyword);
+  }
+
+  deleteHistory(key:any){
+    // alert(key);
+    this.historyList.splice(key,1);
+  }
+
+}
