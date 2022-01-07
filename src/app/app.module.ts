685da5a18b79aa63dad2488ef3ecdc3c4786db1d
Angeliau <42534525+Angeliau@users.noreply.github.com>
2022-01-07 21:24:51 +0800

    提交app根组件源码

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,1 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+// 使用双向数据绑定，必须先引入该模块
+import { FormsModule } from '@angular/forms';
+// echarts
+import { NgxEchartsModule } from 'ngx-echarts';
 
+import { AppComponent } from './app.component';
+import { NewsComponent } from './components/news/news.component';
+import { HomeComponent } from './components/home/home.component';
+import { FormComponent } from './components/form/form.component';
+import { SearchComponent } from './components/search/search.component';
+import { EchartComponent } from './components/echart/echart.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NewsComponent,
+    HomeComponent,
+    FormComponent,
+    SearchComponent,
+    EchartComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    NgxEchartsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
